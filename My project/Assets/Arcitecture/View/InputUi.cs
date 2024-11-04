6504c045890dbaf540ef21da8885efaa49a65fa7
Stephan0404 <stephan906082@gmail.com>
2024-11-04 03:02:04 +0300

    layout View module

---
+++ My project/Assets/Arcitecture/View/InputUi.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputUi : View
+{
+    private  string logoText;
+    private string descriptionText;
+    private string priseText;
+    private string discontText;
+    private List<Objects> objects;
+
+    public override void disc(bool disc)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void setDiscr(string descript)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void setLogo(string logoTest)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void setObj(List<Objects> objected)
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- My project/Assets/Arcitecture/View/View.cs
+++ My project/Assets/Arcitecture/View/View.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public abstract class View : MonoBehaviour
 {
-    public abstract void ColObg(List<Objects> objs);
-    public abstract void logoText(string logoText);
-    public abstract void descriptionText(string descriptionText);
-    public abstract void priseText(string priseText);
-    public abstract void discontText(string discontText);
+    public abstract void disc(bool disc);
+    public abstract void setLogo(string logoTest);
+    public abstract void setDiscr(string descript);
+    public abstract void setObj(List<Objects> objected);
+
 
 
 }
